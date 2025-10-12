f7b7cba84c668c82421f6ac02be56bf27801afdb
Al Faruk <alfaruk2629@gmail.com>
2025-10-12 23:58:58 +0800

    maintenance

--- js/data.js
+++ js/data.js
@@ -1,32 +1,62 @@
 const galleryData = [
   {
     img: "images/img-1.png",
-    name: "Toyota Hiace",
-    price: 850000, // angka aja
+    name: "Avanza",
+    price: 650000, // per hari
+  },
+    {
+    img: "images/img-1.png",
+    name: "Xenia",
+    price: 650000, // per hari
   },
   {
     img: "images/img-2.png",
     name: "Innova Reborn",
-    price: 650000,
+    price: 850000,
   },
   {
     img: "images/img-3.png",
-    name: "Toyota Avanza",
-    price: 450000,
+    name: "Innova Zenix",
+    price: "-", // harga belum tersedia, bisa diganti nanti
   },
   {
-    img: "images/img-3.png",
-    name: "Elf Long",
-    price: 950000,
-  },
-  {
-    img: "images/img-2.png",
-    name: "Fortuner",
+    img: "images/img-4.png",
+    name: "Hiace Commuter (start from)",
     price: 1200000,
   },
   {
-    img: "images/img-1.png",
-    name: "Alphard",
+    img: "images/img-5.png",
+    name: "Hiace Premio (start from)",
     price: 1500000,
+  },
+  {
+    img: "images/img-6.png",
+    name: "Elf 17 seats",
+    price: 1200000,
+  },
+  {
+    img: "images/img-7.png",
+    name: "Elf Giga 19 seats",
+    price: 1400000,
+  },
+  {
+    img: "images/img-8.png",
+    name: "Medium Bus 31/35 seats",
+    price: 2000000,
+  },
+  {
+    img: "images/img-9.png",
+    name: "Medium Bus Long 35/39 seats",
+    price: 2300000,
+  },
+  {
+    img: "images/img-10.png",
+    name: "BigBus 45/50/60 seats",
+    price: 2700000,
+  },
+  {
+    img: "images/img-11.png",
+    name: "BigBus Executive 30/40 seats",
+    price: 3500000,
   },
 ];

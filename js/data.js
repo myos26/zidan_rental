90936e400928a12440fce0f2eeb01d62a9cf208f
Al Faruk <alfaruk2629@gmail.com>
2025-10-25 16:42:57 +0800

    fixing armada

--- js/data.js
+++ js/data.js
@@ -1,51 +1,51 @@
 const galleryData = [
   {
-    img: "images/img-1.png",
+    img: "images/armada/img-1.png",
     name: "Toyota Avanza",
     price: 450000, // per hari
   },
   {
-    img: "images/img-2.png",
+    img: "images/armada/img-2.png",
     name: "Suzuki Ertiga",
     price: 450000, // per hari
   },
   {
-    img: "images/img-3.png",
+    img: "images/armada/img-3.png",
     name: "Innova Zenix",
     price: 550000,
   },
   {
-    img: "images/img-4.png",
+    img: "images/armada/img-4.png",
     name: "Innova Reborn",
     price: 550000,
   },
   {
-    img: "images/img-5.png",
+    img: "images/armada/img-5.png",
     name: "Hiace Commuter (start from)",
     price: 1200000,
   },
   {
-    img: "images/img-6.png",
+    img: "images/armada/img-6.png",
     name: "Hiace Premio (start from)",
     price: 1400000,
   },
   {
-    img: "images/img-7.png",
+    img: "images/armada/img-7.png",
     name: "Alphard",
     price: 2800000,
   },
   {
-    img: "images/ElfLong.jpg",
+    img: "images/armada/ElfLong.jpg",
     name: "Elf Long 17 seats",
     price: 1200000,
   },
   {
-    img: "images/MediumBus.jpg",
+    img: "images/armada/MediumBus.jpg",
     name: "Medium Bus 31–33 seats",
     price: 2000000,
   },
   {
-    img: "images/BigBus.jpg",
+    img: "images/armada/BigBus.jpg",
     name: "Big Bus 50–59 seats",
     price: 3000000,
   },
